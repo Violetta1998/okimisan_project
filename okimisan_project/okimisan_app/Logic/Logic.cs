0207036e0b5bd5e4ae8d221984c716d7b1ca7cf6
Unknown <violettaff@gmail.com>
2018-03-18 14:32:00 +0300

    CreateOrder
    final

--- okimisan_project/okimisan_app/Logic/Logic.cs
+++ okimisan_project/okimisan_app/Logic/Logic.cs
@@ -51,7 +51,7 @@ namespace okimisan_app.Logic
 
             auth.onAuth = () => {
                 if (auth.isAuth)
-                    general.currentPage = General.PAGES.ClientsList;
+                    general.currentPage = General.PAGES.CreateOrder;
             };
 
             auth.onUnAuth = () =>
